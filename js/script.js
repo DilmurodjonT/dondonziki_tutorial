eb1a024ed89af059e85db9db5821653f50733d02
DilmurodjonT <dilmurodjontadjibaev@gmail.com>
2023-05-21 16:38:57 +0500

    end project

--- js/script.js
+++ js/script.js
@@ -1,97 +1,118 @@
-const choices = document.querySelectorAll(".choice"),
-  score = document.querySelector("#score"),
-  modal = document.querySelector(".modal"),
-  result = document.querySelector("#result"),
-  restart = document.querySelector("#restart"),
-  scoreBoard = {
-    player: 0,
-    computer: 0,
-    draw: 0,
-  };
+window.addEventListener("DOMContentLoaded", () => {
+  const choices = document.querySelectorAll(".choice"),
+    score = document.querySelector("#score"),
+    modal = document.querySelector(".modal"),
+    result = document.querySelector("#result"),
+    restart = document.querySelector("#restart"),
+    scoreBoard = {
+      player: 0,
+      computer: 0,
+      draw: 0,
+    };
 
-// Play game
-function play() {
-  restart.style.display = "inline-block";
-  const computerChoice = getComputerChoice();
-}
-
-// GetComputerChoice
-function getComputerChoice() {
-  const rand = Math.random() * 10;
-  if (rand < 3) {
-    return "rock";
-  } else if (rand <= 6) {
-    return "paper";
-  } else {
-    return "scissors";
+  // Play game
+  function play(e) {
+    restart.style.display = "inline-block";
+    const playerChoice = e.target.id;
+    const computerChoice = getComputerChoice();
+    const winner = getWinner(playerChoice, computerChoice);
+    showWinner(winner, computerChoice);
   }
-}
 
-// GetWinner
-function getWinner(p, c) {
-  if (p === c) {
-    return "draw";
-  } else if (p === "rock") {
-    if (c === "paper") {
-      return "computer";
+  // GetComputerChoice
+  function getComputerChoice() {
+    const rand = Math.random() * 10;
+    if (rand < 3) {
+      return "rock";
+    } else if (rand <= 6) {
+      return "paper";
     } else {
-      return "player";
-    }
-  } else if (p === "paper") {
-    if (c === "scissors") {
-      return "computer";
-    } else {
-      return "player";
-    }
-  } else if (p === "scissors") {
-    if (c === "rock") {
-      return "computer";
-    } else {
-      return "player";
+      return "scissors";
     }
   }
-}
 
-// ShowWinner
-function showWinner(winner, computerChoice) {
-  if (winner === "player") {
-    scoreBoard.player++;
-    result.innerHTML = `
+  // GetWinner
+  function getWinner(p, c) {
+    if (p === c) {
+      return "draw";
+    } else if (p === "rock") {
+      if (c === "paper") {
+        return "computer";
+      } else {
+        return "player";
+      }
+    } else if (p === "paper") {
+      if (c === "scissors") {
+        return "computer";
+      } else {
+        return "player";
+      }
+    } else if (p === "scissors") {
+      if (c === "rock") {
+        return "computer";
+      } else {
+        return "player";
+      }
+    }
+  }
+
+  // ShowWinner
+  function showWinner(winner, computerChoice) {
+    if (winner === "player") {
+      scoreBoard.player++;
+      result.innerHTML = `
     <h1 class="text-win">You win</h1>
     <i class="fas fa-hand-${computerChoice} fa-10x"></i>
     <p>Computer Chose <string>${
       computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)
     }</string></p>
     `;
-  } else if (winner === "computer") {
-    scoreBoard.computer++;
-    result.innerHTML = `
+    } else if (winner === "computer") {
+      scoreBoard.computer++;
+      result.innerHTML = `
     <h1 class="text-lose">You lose</h1>
     <i class="fas fa-hand-${computerChoice} fa-10x"></i>
     <p>Computer Chose <string>${
       computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)
     }</string></p>
     `;
-  } else {
-    restart.innerHTML = `
+    } else {
+      result.innerHTML = `
     <h1>It's A Draw</h1>
     <i class="fas fa-hand-${computerChoice} fa-10x"></i>
     <p>Computer Chose <string>${
       computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)
     }</string></p>
     `;
-  }
-  score.innerHTML = `
+    }
+    score.innerHTML = `
   <p> Player: ${scoreBoard.player}</p>
   <p> Computer: ${scoreBoard.computer}</p>
   `;
-  modal.style.display = "block";
-}
+    modal.style.display = "block";
+  }
 
-// RestartGame
-function restartGame() {}
+  // RestartGame
+  function restartGame() {
+    scoreBoard.player = 0;
+    scoreBoard.computer = 0;
+    score.innerHTML = `
+  <p> Player: ${scoreBoard.player}</p>
+  <p> Computer: ${scoreBoard.computer}</p>
+  `;
+  }
 
-// ClearModal
-function clearModal() {}
+  // ClearModal
+  function clearModal(e) {
+    if (e.target == modal) {
+      modal.style.display = "none";
+    }
+  }
 
-// Event listenears
+  // Event listenears
+  choices.forEach((choice) => {
+    return choice.addEventListener("click", play);
+  });
+  window.addEventListener("click", clearModal);
+  restart.addEventListener("click", restartGame);
+});
